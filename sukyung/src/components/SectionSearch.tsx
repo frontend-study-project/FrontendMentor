1bbc357ff4c5483b978dfd1b6763d93c98974faf
sukyung6999 <sukyung6999@gmail.com>
2024-06-26 21:26:23 +0900

    add: input창 빈칸 입력 대응

--- sukyung/src/components/SectionSearch.tsx
+++ sukyung/src/components/SectionSearch.tsx
@@ -1,16 +1,41 @@
+import { useRef, useState } from 'react'
 import classes from './section.module.css'
 
 export default function SectionSearch() {
+  const linkRef = useRef<HTMLInputElement>(null)
+  const [linkInput, setLinkInput] = useState('')
+  const [blankError, setBlankError] = useState(false)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLinkInput(e.target.value)
+  }
+  const handleLinkSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (linkInput === '') {
+      setBlankError(true)
+      linkRef.current?.focus()
+      return
+    }
+
+    setBlankError(false)
+
+    const encodeURL = encodeURIComponent(linkInput.replace(/\s/g, ''))
+  }
+
   return (
     <div className={classes.wrap_search}>
       <div className="inner_cont">
-        <div className={classes.box_search}>
-          <label htmlFor="link" className="screen_out">
-            link
-          </label>
-          <input id="link" type="text" placeholder="Shorten a link here..." />
-          <button className="btn_bg">Shorten It!</button>
-        </div>
+        <form className={classes.box_search} onSubmit={handleLinkSubmit}>
+          <div>
+            <label htmlFor="link" className="screen_out">
+              link
+            </label>
+            <input ref={linkRef} id="link" className={blankError ? classes.type_error : ''} type="text" placeholder="Shorten a link here..." onChange={handleInputChange} />
+            <button className="btn_bg">Shorten It!</button>
+          </div>
+          {blankError && <p className={classes.txt_alert}>Please add a link</p>}
+        </form>
       </div>
     </div>
   )
