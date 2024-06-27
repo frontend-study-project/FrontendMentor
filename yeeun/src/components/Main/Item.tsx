c5c0475f97e1c9b86844a91b57a6a9a73e6b5ef3
yeeun <84173008+jye104@users.noreply.github.com>
2024-06-27 15:46:54 +0900

    fix: key코드 cca2->cca3로 수정

--- yeeun/src/components/Main/Item.tsx
+++ yeeun/src/components/Main/Item.tsx
@@ -4,14 +4,16 @@ const Item = ({ data }) => {
   return (
     <ul>
       {data?.map((c) => (
-        <li key={c.cca2}>
-          <img src={c.flags.png} alt={c.flags.alt} />
-          <h2>{c.name.common}</h2>
-          <div>
-            <p>Population: {c.population}</p>
-            <p>Region: {c.region}</p>
-            <p>Capital: {c.capital}</p>
-          </div>
+        <li key={c.cca3}>
+          <Link to={`/${c.cca3}`}>
+            <img src={c.flags.png} alt={c.flags.alt} />
+            <h2>{c.name.common}</h2>
+            <div>
+              <p>Population: {c.population}</p>
+              <p>Region: {c.region}</p>
+              <p>Capital: {c.capital}</p>
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
