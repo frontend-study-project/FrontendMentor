e35e2c40bbaa79f6e543b3d519bac3c1d16619b0
gayoung <did-619@naver.com>
2024-07-01 16:53:04 +0900

    feat: add detail page

--- gayoung/src/component/contents.tsx
+++ gayoung/src/component/contents.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCountryData } from "../api";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Contents = () => {
   const { data, isLoading, isError } = useQuery({
@@ -76,15 +77,17 @@ const Contents = () => {
         <div className="filter_wrap">
           <ul>
             {searchedData?.map((item) => (
-                <li key={item.name}>
-                  <img src={item.flags.png} alt={item.name}/>
-                  <div className="txt_wrap">
-                    <h2>{item.name}</h2>
-                    <p><span>Population:</span><span>{item.population}</span></p>
-                    <p><span>Region:</span><span>{item.region}</span></p>
-                    <p><span>Capital:</span><span>{item.capital}</span></p>
-                  </div>
-                </li>
+                <Link key={item.name} to={`/country/${item.name}`}>
+                  <li>
+                    <img src={item.flags.png} alt={item.name}/>
+                    <div className="txt_wrap">
+                      <h2>{item.name}</h2>
+                      <p><span>Population:</span><span>{item.population}</span></p>
+                      <p><span>Region:</span><span>{item.region}</span></p>
+                      <p><span>Capital:</span><span>{item.capital}</span></p>
+                    </div>
+                  </li>
+                </Link>
             ))}
           </ul>
         </div>
