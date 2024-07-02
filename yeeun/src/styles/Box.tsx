74c3ea263cd37e5c0a6dab859d73be0b213b97dc
yeeun <84173008+jye104@users.noreply.github.com>
2024-07-02 17:58:16 +0900

    add: 공통 UI - Box 생성

---
+++ yeeun/src/styles/Box.tsx
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react';
+
+const widths = {
+  back: '132px',
+  link: '95px',
+  search: '480px',
+  filter: '200px',
+  countriesList: '265px',
+};
+
+const Box = ({ children, variant, customCss = {} }) => {
+  return (
+    <div
+      css={{
+        display: 'inline-block',
+        padding: '.6em 0',
+        width: widths[variant],
+        borderRadius: '6px',
+        border: '1px solid rgba(27, 31, 36, 0.15)',
+        backgroundColor: 'rgba(246, 248, 250)',
+        color: 'rgba(36, 41, 47)',
+        boxShadow: '0 2px 5px 1px rgba(64, 60, 67, .16)',
+        ...customCss,
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Box;
