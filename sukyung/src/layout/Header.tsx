2ae5608e68c5ffb63a2b56ffa472b69d3a1868a5
sukyung6999 <sukyung6999@gmail.com>
2024-07-02 15:56:02 +0900

    add: 모바일 내비 버튼 동작 추가

--- sukyung/src/layout/Header.tsx
+++ sukyung/src/layout/Header.tsx
@@ -5,9 +5,11 @@ import { useState } from 'react'
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false)
+
   const handleMenuClick = () => {
     setIsOpen((prev) => !prev)
   }
+
   return (
     <header className={classes.head_main}>
       <h1>
@@ -18,7 +20,7 @@ export default function Header() {
       <button type="button" className={classes.btn_menu} onClick={handleMenuClick}>
         <MenuIcon>더보기</MenuIcon>
       </button>
-      <div className={classes.wrap_utils}>
+      <div className={`${classes.wrap_utils} ${isOpen ? classes.active : ''}`}>
         <nav className={classes.nav_main}>
           <h2 className="screen_out">주요 서비스</h2>
           <ul>
