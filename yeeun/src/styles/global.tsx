7bf86127e5963399e19c5a623b72cbe8320ebc98
yeeun <84173008+jye104@users.noreply.github.com>
2024-07-02 18:39:42 +0900

    style: global 스타일 추가

---
+++ yeeun/src/styles/global.tsx
@@ -0,0 +1,41 @@
+import { css } from '@emotion/react';
+import '@fontsource/nunito-sans';
+
+export const globalStyles = css`
+  @import url(https://fonts.googleapis.com/css?family=Nunito+Sans:300,600,800);
+  body {
+    font-family: 'Nunito Sans', '-apple-system', sans-serif;
+    font-size: 14px;
+    font-weight: 300;
+    color: #171311;
+    background: #fafafa;
+  }
+  body,
+  div,
+  h1,
+  h2,
+  ul,
+  button {
+    padding: 0;
+    margin: 0;
+  }
+  li {
+    list-style: none;
+  }
+  button,
+  input {
+    font-family: inherit;
+    font-size: inherit;
+    color: inherit;
+    background: none;
+    cursor: pointer;
+    border: none;
+  }
+  a:link,
+  a:visited,
+  a:hover,
+  a:active {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
