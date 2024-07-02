4e9c64651733f323e3e1519ac45db58245259adc
ohoCoding <youngho7482@gmali.com>
2024-07-02 18:00:28 +0900

    ✏️ App 수정

--- youngho/src/App.tsx
+++ youngho/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStorage from "./contexts/GlobalContext";
+import Home from "./pages/home/index";
+import Details from "./pages/detail/index";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <GlobalStorage>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route
+            path="rest-countries-api-with-color-theme-switcher"
+            element={<Home />} /* this route is only for use on GitHub Pages */
+          />
+          <Route path="/:name" element={<Details />} />
+        </Routes>
+      </GlobalStorage>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
