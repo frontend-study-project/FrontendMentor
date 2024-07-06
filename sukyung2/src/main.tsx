9f9b9ea957f2ce8bf01eedfeebe340491f0f071f
sukyung6999 <sukyung6999@gmail.com>
2024-07-06 23:53:33 +0900

    add: 라우터 설정

--- sukyung2/src/main.tsx
+++ sukyung2/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import MainPage from './assets/pages/MainPage.tsx';
+import DetailPage from './assets/pages/DetailPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+  {
+    path: '/detail/:countryId',
+    element: <DetailPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
